00a9bdaeac45ebe28e52bbdd3d2f51ff661bbdbf
THO-Gaetan <9771@holbertonstudents.com>
2025-03-16 23:26:18 +0100

    delete useless import

--- part3/app/api/v1/amenities.py
+++ part3/app/api/v1/amenities.py
@@ -1,6 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
-from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Namespace('amenities', description='Amenity operations')
 
